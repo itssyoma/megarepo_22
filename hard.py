087b1ecc3d6e1d7c06d957b87c307c2d93147936
itssyoma <itssyoma@gmail.com>
2023-11-22 12:22:55 +0300

    Update hard.py

--- hard.py
+++ hard.py
@@ -4,25 +4,20 @@
 import math
 
 
-def factorial(n):
-    if n == 0:
-        return 1
-    else:
-        return n * factorial(n-1)
-
-
 def sinus(x, eps):
-    sum = 0
-    n = 0
-    while True:
-        term = ((-1)**n * x**(2*n+1)) / (factorial(2*n+1) * (2*n+1))
-        sum += term
-        if math.fabs(sum - term) < eps:
-            break
-    return sum
+    a = x
+    sum, n = a, 1
+
+    # Сумма членов ряда
+    while math.fabs(a) > eps:
+        a *= (-1) * (x**2 * (2*n+1)) / ((2*n+2)*((2*n+3)**2))
+        sum += a
+        n += 1
+
+    return(sum)
 
 
 if __name__ == '__main__':
     x = float(input("Введите значение x: "))
     eps = float(input("Введите желаемую точность: "))
-    print("Значение Sin(x) =", sinus(x, eps))
+    print("Значение Si(x) =", sinus(x, eps))
